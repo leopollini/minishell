9ebb69003af300deaf6f503b06a40e804abefd14
Nazar963 <nizarbonex1@gmail.com>
2023-10-08 11:18:38 +0200

    bonus

--- wildcard/utils.c
+++ wildcard/utils.c
@@ -6,7 +6,7 @@
 /*   By: naal-jen <naal-jen@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 10:38:07 by naal-jen          #+#    #+#             */
-/*   Updated: 2023/10/08 11:11:35 by naal-jen         ###   ########.fr       */
+/*   Updated: 2023/10/08 11:17:41 by naal-jen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,18 +28,21 @@ void	initialize_three_to_zero(int *n, int *i, int *z)
 
 void	pr_dir(char **new_str, char **split_wild, int first, int last_char)
 {
-	// loco()->entry = readdir(loco()->dir);
-	while ((loco()->entry = readdir(loco()->dir)) != NULL)
+	loco()->entry = readdir(loco()->dir);
+	while (loco()->entry != NULL)
 	{
 		if (strcmp(loco()->entry->d_name, ".") == 0
 			|| strcmp(loco()->entry->d_name, "..") == 0)
+		{
+			loco()->entry = readdir(loco()->dir);
 			continue ;
+		}
 		if (match(loco()->entry->d_name, split_wild, first, last_char) != 0)
 		{
 			*new_str = ft_strjoin_free(*new_str, loco()->entry->d_name);
 			*new_str = ft_strjoin_free(*new_str, " ");
 			loco()->flag_no_process = 1;
 		}
-		// loco()->entry = readdir(loco()->dir);
+		loco()->entry = readdir(loco()->dir);
 	}
 }
